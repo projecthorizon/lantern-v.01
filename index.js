e981ab52c1309d81a508fd3ee244d2325daec30d
karlsecco <karlsecco@gmail.com>
2018-08-17 14:57:50 -0700

    completing first v.01 mock

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 function toggleContainer(containerId) {
   const containers = document.querySelectorAll('.sub-container');
-  containers.forEach(function(div) {
-    console.log(div.id);
-    if (containerId === div.id) {
-      console.log('match');
-      div.style.display = 'block';
-    } else div.style.display = 'none';
-  });
+  const subNavLinks = document.querySelectorAll('.sub-nav a');
+  // toggle divs
+  for (let div of containers) {
+    if (containerId === div.id) div.style.display = 'block';
+    else div.style.display = 'none';
+  }
+  // toggle active link
+  for (let a of subNavLinks) {
+    if (containerId === a.dataset.ref) a.style.color = 'rgb(56, 74, 95)';
+    else a.style.color = 'white';
+  }
 }
