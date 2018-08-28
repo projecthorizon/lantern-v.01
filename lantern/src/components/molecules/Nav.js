f1a2bf3d77e696698bed94a4823d08088bd18255
karlsecco <karlsecco@gmail.com>
2018-08-27 17:04:30 -0700

    moving index to survey

---
+++ lantern/src/components/molecules/Nav.js
@@ -0,0 +1,6 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import NavButton from '../../components/atoms';
+
+export default class Nav extends PureComponent {}
